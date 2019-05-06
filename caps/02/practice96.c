84f8e418ec8e18992e4fa7ec136258d10bea9971
Fei Li <wizard@pursuetao.com>
2019-05-06 12:16:42 +0800

    feat: chapter 02 hw float 96 97

---
+++ caps/02/practice96.c
@@ -0,0 +1,24 @@
+/*
+ * Practice 96
+ *
+ * #+AUTHOR: Fei Li
+ * #+EMAIL: wizard@pursuetao.com
+ */
+#include <assert.h>
+
+#include "float_bits.h"
+
+/* Compute int(f)
+ * If conversion causes overflow or f is NaN, return 0x80000000.
+ */
+int float_f2i(float_bits f);
+
+int main(void)
+{
+        return 0;
+}
+
+int float_f2i(float_bits f)
+{
+        
+}
---
+++ caps/02/practice97.c
@@ -0,0 +1,22 @@
+/*
+ * Practice 97
+ *
+ * #+AUTHOR: Fei Li
+ * #+EMAIL: wizard@pursuetao.com
+ */
+#include <assert.h>
+
+#include "float_bits.h"
+
+/* Compute float(i) */
+float_bits float_i2f(int i);
+
+int main(void)
+{
+        return 0;
+}
+
+float_bits float_i2f(int i)
+{
+        
+}
